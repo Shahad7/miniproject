61eb2aba109c9863ed39487250f46cc1fe55afd8
shahad7 <shahadneeliyatt786@gmail.com>
2023-06-15 23:28:59 +0530

    migrate db,add rent etc

--- hello/admin.py
+++ hello/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Book,myuser,Library,Rent
+from .models import Book,myuser,Rent
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("id","title","author","photo","stock","libid")
+    list_display = ("id","title","author","photo","stock")
 class userAdmin(admin.ModelAdmin):
-    list_display = ("id","username","name","email","role","libid")
+    list_display = ("id","username","name","email","role")
 class LibraryAdmin(admin.ModelAdmin):
     list_display = ("id","name")
 class RentAdmin(admin.ModelAdmin):
@@ -14,5 +14,5 @@ class RentAdmin(admin.ModelAdmin):
 admin.site.register(Book,BookAdmin)
 admin.site.register(myuser,userAdmin)
 admin.site.register(Rent,RentAdmin)
-admin.site.register(Library,LibraryAdmin)
+
 
